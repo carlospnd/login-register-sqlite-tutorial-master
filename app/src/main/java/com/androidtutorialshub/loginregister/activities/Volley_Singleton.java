5479bf895c159e62fa7dbaf9eaddabff8c31c681
Cristian Azurdia <201020331@ingenieria.usac.edu.gt>
2017-04-27 09:42:23 -0600

    Versión 0.5.3
    -Corección de Errores

--- app/src/main/java/com/androidtutorialshub/loginregister/activities/Volley_Singleton.java
+++ app/src/main/java/com/androidtutorialshub/loginregister/activities/Volley_Singleton.java
@@ -15,7 +15,7 @@ public final class Volley_Singleton {
     private static RequestQueue request;
     private static Context context;
 
-    private static final String url_connect = "http://192.168.1.4:5000/";;
+    private static final String url_connect = "http://172.20.10.4:5000/";;
 
     private Volley_Singleton(Context contexto){
         Volley_Singleton.context = contexto;
